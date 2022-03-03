2dd73e10e2c0337c1357607623b7167152f029ac
doritohead <81892751+doritohead@users.noreply.github.com>
2022-03-03 23:52:38 +0000

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,162 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateUseableItem("chain_v", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_V", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_v1", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_V1", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_v2", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_V2", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_bdb", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_bdb", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_cp1", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_cp1", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_cp2", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_cp2", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_skull", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_skull", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_skull2", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_skull2", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_skull3", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_skull3", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_skull4", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_skull4", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_skull5", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_skull5", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_skull6", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_skull6", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_skull7", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_skull7", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_nocap", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_nocap", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_snake", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_snake", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_link", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_link", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au1", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au1", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au2", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au2", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au3", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au3", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au4", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au4", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au5", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au5", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au6", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au6", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au7", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au7", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au8", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au8", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au9", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au9", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au10", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au10", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au11", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au11", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au12", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au12", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au13", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au13", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au14", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au14", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au15", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au15", src, item.name)
+end)
+QBCore.Functions.CreateUseableItem("chain_au16", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent("chains:client:chain_au16", src, item.name)
+end)
